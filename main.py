e192ec940a45c6d7d17033e6e17a456612bb4ccf
RodionQA <r.hoshabov@itsupp24.ru>
2024-02-17 17:42:04 +0400

    1

---
+++ main.py
@@ -0,0 +1,92 @@
+# print("Hello", "world", end=", ")
+# print("Hello" "world")
+
+# input("Please put some text here")
+# name = input("Enter your name")
+# print(name)
+
+# name, age = "Rodik", 34
+# print(name, age)
+
+# name = "Rodion"
+# other_name = name
+# print(other_name)
+
+# x = 666
+# y = 888
+#
+# x, y = y, x
+# print(x, y)
+
+# variable1 = 1
+# variable2 = "Hi!"
+# variable3 = None
+# variable4 = 1.23
+# variable5 = 0
+# print(type(variable5))
+
+# my_integer = 9
+# my_float = 4
+#
+# summary1 = my_integer + my_float
+# summary2 = my_integer - my_float
+# summary3 = my_integer * my_float
+# summary4 = my_integer / my_float
+# print(my_float ** 3) Возведение в степень
+# print(my_float * my_float * my_float) Эквиваленто этому
+# print(my_integer // my_float) Целочисленное диление - на сколько целых раз делится число
+# print(my_integer % my_float) Остаток от целочисленного деления
+
+# integer = (2 + 3) * 5 первым будет сложение потом умножение
+
+# big_number = 10 ** 1000
+# print(big_number)
+
+# my_int = 1
+# my_float = float(my_int) Превращаем int(Число) в float(Дробное число)
+# print(my_float)
+
+# my_float = 1.6666666
+# my_int = int(my_float) делает из Float Int
+# print(my_int)
+#
+# my_float = 1.6666666
+# my_int = round(my_float) округляет число в большую сторону
+# print(my_int)
+
+# tower = 5 * 20
+# floor = 4
+# print(tower)
+
+# print(bool(1)) - True
+# print(bool(0)) - False
+# print(bool(-1)) - True
+
+# print(bool("")) - False
+# print(bool("PoP!")) - True
+
+
+# Условия
+#
+# if False:
+#     print("Hello")
+# else:
+#     print("Hello Rodik")
+#
+# x = 10
+# if x > 0:
+#     print("x is positive") Выполнится первое условие
+# elif x > 1:
+#     print("x is negative")
+# else:
+#     print("x is zero")
+
+x = 10
+y = 20
+
+# if x > 0: Не правильно
+#     if y > 0:
+#         print("x and y is positive")
+
+# if x > 0 and y > 0: Правильно
+#         print("x and y is positive")
